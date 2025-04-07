5ffa7d5dfcdfae069523667fce0bb95fd2c5736f
Phuoctran96 <tranhoangphuoc18@gmail.com>
2025-04-07 17:07:12 +0700

    style dep 	modified:   client/src/components/Header.jsx 	modified:   client/src/styles.css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 const Header = ({ onLogout }) => {
   return (
     <header className="header">
-      <h1>Route Finder</h1>
-      <p>Made from React by Hoàng Phước with love</p>
-      <button onClick={onLogout}>Đăng xuất</button>
+      <div className="header-content">
+        <h1>Route Finder</h1>
+        <p>Made from React by Hoàng Phước with love</p>
+      </div>
+      <button className="logout-button" onClick={onLogout}>
+        <i className="fas fa-sign-out-alt"></i> Đăng xuất
+      </button>
     </header>
   );
 };
